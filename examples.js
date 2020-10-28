726a5dba4826b3f133622fca237f62981568a0a2
Karl Stolley <karl.stolley@gmail.com>
2020-10-28 15:49:58 -0500

    Add HTML and JavaScript, w/ examples from Monday

---
+++ examples.js
@@ -0,0 +1,21 @@
+console.log('Hello from JavaScript!');
+
+// Refreshers from Monday...
+// We can have numbers...
+console.log(5, 'is a number...');
+// We can have true/false...
+console.log(true, 'and', false, 'are Booleans...');
+// We can have strings
+console.log('"12345"', 'and', '"abcdef"', 'are strings...');
+// We can have null values
+console.log(null, 'is null...')
+// We can have undefined values
+console.log(undefined, 'is undefined...')
+// We can have NaN (Not a Number) values
+console.log(5/"cow", 'is not a number...');
+
+// Complex values!
+// We can have arrays
+console.log(['a','b','c'], 'is an array...');
+// We can have object literals
+console.log({one: 'two', buckle: 'my shoe'}, 'is an object...');
